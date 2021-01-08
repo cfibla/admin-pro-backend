9de1e57527460e9ccd958455988a50fea5ee5d96
Carlos Fibla <charliesuper@gmail.com>
2021-01-08 16:44:58 +0100

    Fin sección 18

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
 const { googleVerify } = require('../helpers/google-verify');
+const { getMenuFrontEnd } = require('../helpers/menu-frontend');
 
 const login = async(req, res = response) => {
 
@@ -34,8 +35,8 @@ const login = async(req, res = response) => {
 
         res.json({
             ok: true,
-            msg: 'Login correcto',
-            token
+            token,
+            menu: getMenuFrontEnd(usuarioDB.role)
         });
 
     } catch (error) {
@@ -81,7 +82,8 @@ const googleSignIn = async(req, res = response) => {
 
         res.json({
             ok: true,
-            token
+            token,
+            menu: getMenuFrontEnd(usuario.role)
         });
 
     } catch (error) {
@@ -108,7 +110,8 @@ const renewToken = async(req, res = response) => {
     res.json({
         ok: true,
         token,
-        usuario
+        usuario,
+        menu: getMenuFrontEnd(usuario.role)
     });
 };
 
