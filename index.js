a50e0a0bb7605fc1c04174d914ed4d4ab736d42e
Carlos Fibla <charliesuper@gmail.com>
2021-01-09 17:54:19 +0100

    Corrección rutas SPA

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Para leer variables de entorno
 require('dotenv').config();
 
+const path = require('path');
+
 const express = require('express');
 // CORS => Aceptar peticines de diferentes dominios
 const cors = require('cors');
@@ -32,6 +34,11 @@ app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/login', require('./routes/auth'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/uploads', require('./routes/uploads'));
+
+// Si no encuentra ninguna ruta
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'))
+})
 
 // Arranca el servidor
 app.listen(process.env.PORT, () => {
